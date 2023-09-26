453f83c0732a6cab0803fbca05ef32b67d5c1c57
sorryShady <akhilnishadofficial@gmail.com>
2023-09-26 11:33:53 +0530

    hero section
    Completed hero section

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,9 +1,34 @@
 import React from 'react'
 import styles from './HeroSection.module.css'
+import message from '../../Assets/Message.png'
+import heroImg from '../../Assets/Hero.png'
 const HeroSection = () => {
   return (
-    <div>
-      
+    <div className={styles.heroContainer} id='#home'>
+      <div className={styles.infoContainer}>
+        <p className={styles.titleOne}>You don't have to</p>
+        <p className={styles.titleTwo}>Fight them Alone.</p>
+        <p className={styles.summary}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut
+          pellentesque ligula. In imperdiet condimentum urna, ac facilisis augue
+          blandit sit amet. Nam vitae gravida metus. Sed at enim lacinia, mattis
+          dolor ut, condimentum ex.
+        </p>
+        <div className={styles.cta}>
+          <div className={styles.mailContainer}>
+            <img src={message} alt='mail' className={styles.mail} />
+            <input
+              type='email'
+              placeholder='Enter your email address'
+              className={styles.mailInp}
+            />
+            <button className={styles.mailBtn}>Let's Talk</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <img src={heroImg} alt='heroImg' className={styles.image} />
+      </div>
     </div>
   )
 }
